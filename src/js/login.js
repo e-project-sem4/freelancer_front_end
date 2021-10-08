72c1b2de11b8289473f11b01497a600aa9ee3781
Windows 10 <trinhlhth1908021@fpt.edu.vn>
2021-10-08 22:30:49 +0700

    fix ajax login

---
+++ src/js/login.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+    $('#login').on("click",function() {
+        const name = $("#username").val();
+        const pass = $("#pass").val();
+        const sendInfo = {
+            username: name,
+            password: pass,
+        };
+        $.ajax({
+            type: 'POST',
+            url: "http://localhost:8081/api/v1/users/login",
+            contentType: "application/json; charset=utf-8",
+            data:JSON.stringify(sendInfo) ,
+            traditional: true,
+            success: function (data) {
+                console.log(data);
+            },
+            error(){
+                console.log("Login sai");
+            },
+        });
+    })
+
+})
