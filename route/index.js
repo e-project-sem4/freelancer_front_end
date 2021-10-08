b80bd71cbf10a03449e51e6e6e555399dc007e31
PhuDepTrai2001 <phulltth1908010@fpt.edu.vn>
2021-10-08 14:13:33 +0700

    sửa menu,tách menu(header) ra view riêng

--- route/index.js
+++ route/index.js
@@ -9,7 +9,86 @@ router.get("/about", (req, res) => {
 });
 
 router.get("/home", (req, res) => {
-    fs.readFile("./src/home.html", "utf-8", (err, data) => {
+    fs.readFile("./src/index.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/job-list", (req, res) => {
+    fs.readFile("./src/job-list.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/job-details", (req, res) => {
+    fs.readFile("./src/job-details.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/candidate-list", (req, res) => {
+    fs.readFile("./src/candidates-listing.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/candidate-details", (req, res) => {
+    fs.readFile("./src/candidates-profile.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/employer-list", (req, res) => {
+    fs.readFile("./src/employers-list.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/employer-details", (req, res) => {
+    fs.readFile("./src", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+
+router.get("/about", (req, res) => {
+    fs.readFile("./src/about.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/proposal-list", (req, res) => {
+    fs.readFile("./src/proposal-list.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/service", (req, res) => {
+    fs.readFile("./src/services.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/team", (req, res) => {
+    fs.readFile("./src/team.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/faq", (req, res) => {
+    fs.readFile("./src/faq.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/contact", (req, res) => {
+    fs.readFile("./src/contact.html", "utf-8", (err, data) => {
+        res.type("text/html");
+        res.send(data);
+    });
+});
+router.get("/pricing", (req, res) => {
+    fs.readFile("./src/pricing.html", "utf-8", (err, data) => {
         res.type("text/html");
         res.send(data);
     });
