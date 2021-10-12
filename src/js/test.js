bb19844effcf21a0a61aaf9e365a02189efbe8df
PhuDepTrai2001 <phulltth1908010@fpt.edu.vn>
2021-10-12 19:53:48 +0700

    update code

--- src/js/test.js
+++ src/js/test.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    if ( localStorage.getItem("user-info") === null)  {
+    if ( localStorage.getItem("user-info") === null)   {
         document.getElementById("logout").innerHTML="Login";
         document.getElementById("logout").setAttribute("href", "/login");
     }
