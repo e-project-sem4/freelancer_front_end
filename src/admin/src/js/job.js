ff5c1defbfa0d0f04bcc02d00f16a7dc5df873de
Windows 10 <trinhlhth1908021@fpt.edu.vn>
2021-10-22 11:08:58 +0700

    fix job list admin

--- src/admin/src/js/job.js
+++ src/admin/src/js/job.js
@@ -27,24 +27,30 @@ function loadAllJob(searchKey, page, pageSize, sort, complexity, skill) {
     async: false,
     success: function (res) {
       const jobList = res.result;
-      console.log(jobList);
+      console.log(jobList)
       totalPage = res.total;
       let itemHtml = "";
       let itemTempHtml = "";
       for (let i = 0; i < jobList.length; i++) {
         var d = new Date(jobList[i].createAt).toLocaleDateString();
+        if(jobList[i].isPaymentStatus ==1){
+          str = '<span class="badge badge-pill badge-primary">Đã thanh toán</span>'
+        }else{
+          str = '<span class="badge badge-pill badge-secondary">Chưa thanh toán</span>'
+        }
+        ;
         itemTempHtml += `
                     <div class="card d-flex flex-row mb-3">
                         <div class="d-flex flex-grow-1 min-width-zero">
                             <div class="card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
-                                <a class="list-item-heading mb-1 truncate w-40 w-xs-100" href="Layouts.Details.html">
+                                <a class="list-item-heading mb-1 truncate w-20 w-xs-100" href="Layouts.Details.html">
                                 ${jobList[i].name}
                                 </a>
-                                <p class="mb-1 text-muted text-small w-15 w-xs-100">Cakes</p>
+                                <p class="mb-1 text-muted text-small w-15 w-xs-100">${jobList[i].userBusiness.user.fullName} </p>
                                 <p class="mb-1 text-muted text-small w-15 w-xs-100">${d}</p>
-                                <div class="w-15 w-xs-100">
-                                    <span class="badge badge-pill badge-secondary">${jobList[i].isPaymentStatus}</span>
-                                </div>
+                                <div class="w-15 w-xs-100">`
+                               + str+
+                                `</div>
                             </div>
                             <div class="custom-control custom-checkbox pl-1 align-self-center pr-4">
                                 <label class="custom-control custom-checkbox mb-0">
@@ -120,15 +126,21 @@ function loadAllJob(searchKey, page, pageSize, sort, complexity, skill) {
 //   event.preventDefault();
 // });
 
-// function changePage() {
-//   page = 1;
-//   pageSize = $("#dropdown-page").val();
-//   start = page * pageSize - pageSize + 1;
-//   end = pageSize * page;
-//   $('#totalResult').html(`${start}-${end}`)
-//   loadAllJob(search, page, pageSize, sort, complexity, skill);
-// }
+function changePage() {
+  page = 1;
+  pageSize = $("#dropdown-page").val();
 
+  start = page * pageSize - pageSize + 1;
+  end = pageSize * page;
+  $('#totalResult').html(`${start}-${end}`)
+  loadAllJob(search, page, pageSize, sort, complexity, skill);
+}
+function changeSort() {
+  page = 1;
+  sort = $("#dropdown-sort").val();
+  loadAllJob(search, page, pageSize, sort, complexity, skill);
+
+}
 // function changeComplexity(data) {
 //   complexity = data;
 //   if (data === null) {
@@ -136,12 +148,7 @@ function loadAllJob(searchKey, page, pageSize, sort, complexity, skill) {
 //   }
 //   loadAllJob(search, page, pageSize, sort, complexity, skill);
 // }
-function changeSort() {
-  page = 1;
-  sort = $('#dropdown-sort').val();
-  loadAllJob(search, page, pageSize, sort, complexity, skill);
 
-}
 
 // function changeSkill() {
 //   const arrSkill = [...document.querySelectorAll(".checkbox-d")].filter(x => x.checked === true).map(e => +e.value).join(",");
