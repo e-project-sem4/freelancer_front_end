a3dee1ee990c650e38545dfaa8d1eee43c962ae5
PhuDepTrai2001 <phulltth1908010@fpt.edu.vn>
2021-10-31 17:01:22 +0700

    up

--- src/js/changepassword.js
+++ src/js/changepassword.js
@@ -1,61 +1,68 @@
-$(document).ready(function() {
-    $('#change-pass').on("click", function(event) {
-        const name =JSON.parse(localStorage.getItem('user-info')).username
+$(document).ready(function () {
+    $('#error1').hide();
+    $('#error2').hide();
+    $('#change-pass').on("click", function (event) {
+
+        const name = JSON.parse(localStorage.getItem('user-info')).username
         const pass = $("#pass").val();
         const passNew = $("#passNew").val();
         const url = baseUrl + `/api/v1/users/changepassword`;
-        const token=  localStorage.getItem('access-token')
+        const token = localStorage.getItem('access-token')
         const param = {
             username: name,
             oldPassword: pass,
             newPassword: passNew,
         };
-        $.ajax({
-            type: 'PATCH',
-            url: url,
-            contentType: "application/json; charset=utf-8",
-            beforeSend: function (xhr){
-                xhr.setRequestHeader(
-                    "Authorization", token
-                );
-            },
-            data:JSON.stringify(param),
-            dataType:"JSON",
-            async: false,
-            success: function(res) {
-               console.log(res.message)
-            },
-        });
-        event.preventDefault()
-    })
-    $(".passwordChange_form").validate({
-        rules: {
-            pass: {
-                required: true,
-                minlength: 8,
-                equalTo: "#pass"
-            },
-            
-            passNew: {
-                required: true,
-                minlength: 8,
-               
-            },
-           
-        },
-        messages: {
-            pass: {
-                required: "Please provide a password",
-                minlength: "Your password must be at least 8 characters long"
-            },
-            passNew: {
-                required: "Please provide a password",
-                minlength: "Your password must be at least 8 characters long",
-               
-            },
-           
-        },
-        
-    });
 
+        if ((pass != "" && pass.length >= 6) && (passNew != "" && passNew.length >= 6)) {
+            $('#error1').hide();
+            $('#error2').hide();
+            $.ajax({
+                type: 'PATCH',
+                url: url,
+                contentType: "application/json; charset=utf-8",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader(
+                        "Authorization", token
+                    );
+                },
+                data: JSON.stringify(param),
+                dataType: "JSON",
+                async: false,
+                success: function (res) {
+                    if (res.status == -1) {
+                        swal(res.message, {
+                            icon: "warning",
+                            buttons: false
+                        });
+                    }
+                    else {
+                        swal("Hooray!", {
+                            icon: "success",
+                            buttons: false,
+                           
+                        });
+                        setTimeout(() => window.location.href = '/login', 2000);
+                    }
+
+                },
+                error: function (res) {
+                }
+            });
+            event.preventDefault()
+        }
+         if (pass == "" && pass.length < 6) {
+            $('#error1').text("Atleast 6 characters")
+            $('#error1').show();
+           
+        }
+         if (passNew == "" && passNew.length < 6) {
+            $('#error2').text("Atleast 6 characters")
+            $('#error2').show();
+            
+        }
+        
+        
+        return
+    })
 })
